5143e1d316ca8f1a648d542355cbd1e84ed52d4b
dave gumba <dave_gumba@yahoo.com>
2020-06-14 13:44:22 -0400

    bare bonesss

---
+++ script.js
@@ -0,0 +1,28 @@
+let input = document.querySelector('input');
+let feed = document.getElementById('content-list');
+ 
+input.addEventListener('change', () => {
+    let files = input.files;
+ 
+    if(files.length == 0) return;
+ 
+    const file = files[0];
+ 
+    let reader = new FileReader();
+ 
+    reader.onload = (e) => {
+        const file = e.target.result;
+        const lines = file.split(/\r\n|\n/);
+        lines.sort(() => Math.random() - 0.5);
+        for(line of lines) {
+            let sentence = '<li>' + line + '</li>';
+            feed.innerHTML += sentence;
+        }
+        console.log(lines);
+    };
+ 
+    reader.onerror = (e) => alert(e.target.error.name);
+ 
+    reader.readAsText(file); 
+    
+});
